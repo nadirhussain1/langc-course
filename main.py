d69b28b1b8ba90e729ec0e5f57e207151e9d2964
Nadir Hussain <nadirhussain1@gmail.com>
2026-05-16 13:09:40 +0500

    learning core concepts

--- main.py
+++ main.py
@@ -1,5 +1,25 @@
+from dotenv import load_dotenv
+load_dotenv()
+
+from importlib.metadata import version 
+core_version=version("langchain")
+lg_version = version("langgraph")
+
+from langchain_openai import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
+
+
+print(f"langchain-core: {core_version}")
+print(f"langgraph-version: {lg_version}")
+
+
 def main():
-    print("Hello from langc-course!")
+
+    llm = ChatAnthropic(model="claude-sonnet-4-5-20250929",temperature=0)
+    claudeResponse=llm.invoke("Say 'Environment Setup' in one word")
+
+    print(f"Response from Claude: {claudeResponse}")
+
 
 
 if __name__ == "__main__":
